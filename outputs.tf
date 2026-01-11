2e1be8060c85b49d48063cf9aef6417eeb4f43f7
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:41:13 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "data_protection_backup_instance_kubernetes_clusters" {
+  description = "All data_protection_backup_instance_kubernetes_cluster resources"
+  value       = azurerm_data_protection_backup_instance_kubernetes_cluster.data_protection_backup_instance_kubernetes_clusters
+}
+
