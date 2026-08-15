e0004052a2b81cd48cb2997cbb2de64261a2f4f7
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:02:54 +0100

    Update to schema version v4.81.0

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,7 @@ output "data_protection_backup_instance_kubernetes_clusters_id" {
 }
 output "data_protection_backup_instance_kubernetes_clusters_backup_datasource_parameters" {
   description = "Map of backup_datasource_parameters values across all data_protection_backup_instance_kubernetes_clusters, keyed the same as var.data_protection_backup_instance_kubernetes_clusters"
-  value       = { for k, v in azurerm_data_protection_backup_instance_kubernetes_cluster.data_protection_backup_instance_kubernetes_clusters : k => v.backup_datasource_parameters if v.backup_datasource_parameters != null && length(v.backup_datasource_parameters) > 0 }
+  value       = { for k, v in azurerm_data_protection_backup_instance_kubernetes_cluster.data_protection_backup_instance_kubernetes_clusters : k => one(v.backup_datasource_parameters) if v.backup_datasource_parameters != null && length(v.backup_datasource_parameters) > 0 }
 }
 output "data_protection_backup_instance_kubernetes_clusters_backup_policy_id" {
   description = "Map of backup_policy_id values across all data_protection_backup_instance_kubernetes_clusters, keyed the same as var.data_protection_backup_instance_kubernetes_clusters"
